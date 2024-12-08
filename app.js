408a2dde8daccf85fb2d9fdf5a58e33c89885f7c
Sadiq Ahmed Khan <155924947+CodeWithKHAN1225@users.noreply.github.com>
2024-12-08 19:16:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: ['Wish You Many more', 'HAPPY', 'Returns of the Day','Abbu💙' ],
+			strings: ['Wish You Many more', 'HAPPY', 'Returns of the Day','Abbu' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
